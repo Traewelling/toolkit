bfe7cd39b79e73d983bc5f26faa3480804402f1b
Levin herr <github@17d.me>
2022-03-31 14:38:11 +0200

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
+import App from "@/App";
+import HelloWorld from "@/components/HelloWorld";
+
+Vue.use(VueRouter)
+
+let router = new VueRouter({
+    routes: [
+        { path: '/lookup', component: HelloWorld },
+        { path: '*', redirect: '/lookup' }
+    ]
+})
 
 Vue.config.productionTip = false
-
+Vue.use(VueRouter);
 new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+    el: '#app',
+    vuetify,
+    render: h=> h(App),
+    router,
+    data: () => ({
+    })
+})
